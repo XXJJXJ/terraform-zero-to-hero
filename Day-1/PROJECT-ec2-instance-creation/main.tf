a742179a46943de7db9892d2eb5761a21932a9ec
Ye Xinjian <e0544100@u.nus.edu>
2024-07-22 22:20:39 +0800

    Day 1

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,10 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+    region = "ap-southeast-1"  # Set your desired AWS region
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    ami           = "ami-0e97ea97a2f374e3d"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
+    subnet_id = "subnet-0c4ebcf47b91b7096"
+    key_name = "aws login"
 }
